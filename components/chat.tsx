fc4380c7abc63c74fdee1003620a4dec1379703f
Marius Stein <Marius.Stein@Viadee.de>
2024-02-22 21:45:28 +0100

    fix navigation on new chat

--- components/chat.tsx
+++ components/chat.tsx
@@ -52,7 +52,8 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
       },
       onFinish() {
         if (!path.includes('chat')) {
-          window.history.pushState({}, '', `/chat/${id}`)
+          router.push(`/chat/${id}`);
+          router.refresh();
         }
       }
     })
