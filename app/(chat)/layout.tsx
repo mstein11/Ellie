df71acea4c18743d249f2970b58c6190ce532134
Marius Stein <mariusstein7@gmail.com>
2024-02-29 23:47:29 +0100

    fix for lg

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -10,7 +10,7 @@ export default async function ChatLayout({ children }: ChatLayoutProps) {
   return (
     <div className="relative flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
       <SidebarDesktop isRight={false} />
-      <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px] xl:pr-[500px] {useRightSideOpen() ? '' : ''}">
+      <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px] lg:pr-[500px]">
         {children}
       </div>
 
