28c9882eb178680e43dbc8d0e23f1895fa4e5967
Marius Stein <mariusstein7@gmail.com>
2024-03-02 16:26:04 +0100

    improve tests

--- __tests__/components/sidebar-list-client.test.js
+++ __tests__/components/sidebar-list-client.test.js
@@ -7,26 +7,28 @@ import { render, screen } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
 
 import { getChats } from './../../app/actions'
+import { SidebarItems } from '../../components/sidebar-items'
+
 jest.mock('./../../app/actions.ts', () => {
   return { getChats: jest.fn() }
 })
+
 jest.mock('./../../lib/utils.ts', () => {
   return { cn: jest.fn() }
 })
+
 jest.mock('./../../components/sidebar-items', () => {
   return {
-    SidebarItems: props => (
-      <div
-        data-testid="sidebaritems"
-        data-test-length={props.chats.length}
-      ></div>
-    )
+    SidebarItems: jest.fn()
   }
 })
 
 import { SidebarListClient } from './../../components/sidebar-list-client'
 
 describe('should load the sidebar list', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
   it('should display No Chat History if empty', async () => {
     getChats.mockReturnValue([])
     await act(async () => render(<SidebarListClient />))
@@ -36,22 +38,18 @@ describe('should load the sidebar list', () => {
   })
 
   it('should load more chats by click on button', async () => {
-    //create an array with 10 entries
-
-    const exampleChats = new Array(10).fill({ id: '1', name: 'Chat 1' }, 0, 10)
-    getChats.mockResolvedValue(exampleChats)
+    const exampleChats = new Array(10).fill("someChatObj", 0, 10)
+    getChats.mockResolvedValue([...exampleChats])
+    
     await act(async () => render(<SidebarListClient />))
 
     expect(getChats).toHaveBeenCalledWith(undefined, 0, 10)
-    expect(
-        screen.getByTestId('sidebaritems').getAttribute('data-test-length')
-      ).toBe('10')
+    expect(SidebarItems).toHaveBeenCalledWith({ chats: new Array(10).fill("someChatObj", 0, 10) }, expect.anything())
 
     await act(async () => screen.getByText('Load more').click())
 
     expect(getChats).toHaveBeenCalledWith(undefined, 10, 20)
-    expect(
-      screen.getByTestId('sidebaritems').getAttribute('data-test-length')
-    ).toBe('20')
+    expect(SidebarItems).toHaveBeenCalledWith({ chats: new Array(20).fill("someChatObj", 0, 20) }, expect.anything())
+
   })
 })
