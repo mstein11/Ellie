0bc233c004d9bb1be2c95b10c253a573bfd1f0f6
Marius Stein <mariusstein7@gmail.com>
2024-04-12 19:55:07 +0200

    WIP doc splitting

--- lib/vectorstore/dataloader.test.ts
+++ lib/vectorstore/dataloader.test.ts
@@ -413,12 +413,117 @@ describe('should test dataloader', () => {
     // expect(largeDocs).toMatchSnapshot()
 
     expect(
+      result
+        .filter((item: any) => !item.children)
+        .sort((a: any, b: any) => a.startInDoc - b.startInDoc)
+        .map((item: any) => item.data)
+        .join('\n---xxx---\n')
+    ).toMatchSnapshot()
+
+    expect(
       res.sort((a: any, b: any) => a.data.length - b.data.length)
     ).toMatchSnapshot()
     expect({
       length: res.length,
       lengthGreater1000: largeDocs.length
     }).toMatchSnapshot()
-})
-  
+  })
+
+  it('should not loose text before table', async () => {
+    const text = `SomeTextBeforeTable
+| Level | Proficiency Bonus |
+|-------|-------------------|
+| 1st   | +2                |`
+
+    const result = await loadSourceV2({
+      maxLength: 50,
+      idProvider: () => 'some-test-id-matching-schema',
+      data: text
+    })
+
+    expect(result.length).toBe(2)
+    expect(result[0].data).toBe('SomeTextBeforeTable\n')
+    expect(result[1].data).toBe(
+      '| Level | Proficiency Bonus |\n|-------|-------------------|\n| 1st   | +2                |'
+    )
+  })
+
+  it("test splitting for large table", async () => {
+    const text = `
+Fighter
+=======
+
+Class Features
+--------------
+
+As a fighter, you gain the following class features.
+
+#### Hit Points
+
+**Hit Dice:** 1d10 per fighter level
+
+**Hit Points at 1st Level:** 10 + your Constitution modifier
+
+**Hit Points at Higher Levels:** 1d10 (or 6) + your Constitution modifier per fighter level after 1st
+
+#### Proficiencies
+
+**Armor:** All armor, shields
+
+**Weapons:** Simple weapons, martial weapons
+
+**Tools:** None
+
+**Saving Throws:** Strength, Constitution
+
+**Skills:** Choose two skills from Acrobatics, Animal
+
+Handling, Athletics, History, Insight, Intimidation, Perception, and Survival
+
+#### Equipment
+
+You start with the following equipment, in addition to the equipment granted by your background:
+- (*a*) chain mail or (*b*) leather armor, longbow, and 20 arrows
+- (*a*) a martial weapon and a shield or (*b*) two martial weapons
+- (*a*) a light crossbow and 20 bolts or (*b*) two handaxes
+- (*a*) a dungeoneer’s pack or (*b*) an explorer’s pack
+
+#### The Fighter
+
+| Level | Proficiency Bonus | Features                                          |
+|-------|-------------------|---------------------------------------------------|
+| 1st   | +2                | Fighting Style, Second Wind                       |
+| 2nd   | +2                | Action Surge (one use)                            |
+| 3rd   | +2                | Martial Archetype                                 |
+| 4th   | +2                | Ability Score Improvement                         |
+| 5th   | +3                | Extra Attack                                      |
+| 6th   | +3                | Ability Score Improvement                         |
+| 7th   | +3                | Martial Archetype Feature                         |
+| 8th   | +3                | Ability Score Improvement                         |
+| 9th   | +4                | Indomitable (one use)                             |
+| 10th  | +4                | Martial Archetype Feature                         |
+| 11th  | +4                | Extra Attack (2)                                  |
+| 12th  | +4                | Ability Score Improvement                         |
+| 13th  | +5                | Indomitable (two uses)                            |
+| 14th  | +5                | Ability Score Improvement                         |
+| 15th  | +5                | Martial Archetype Feature                         |
+| 16th  | +5                | Ability Score Improvement                         |
+| 19th  | +6                | Ability Score Improvement                         |
+| 20th  | +6                | Extra Attack (3)                                  |`;
+
+    const result = await loadSourceV2({
+      idProvider: () => 'some-test-id-matching-schema',
+      data: text
+    });
+
+    expect(
+        result
+          .filter((item: any) => !item.children)
+          .sort((a: any, b: any) => a.startInDoc - b.startInDoc)
+          .map((item: any) => item.data)
+          .join('\n---xxx---\n')
+      ).toMatchSnapshot()
+
+      expect(result.length).toBe(7);
+  });
 })
--- lib/vectorstore/dataloader.ts
+++ lib/vectorstore/dataloader.ts
@@ -121,14 +121,18 @@ export async function prepareLvl1Headings(
   const pattern = /.+\n=+\n/g
   const res = getPositionsOfNeedle(inputString, pattern)
 
+  if (res.some(item => item.start !== 0)) {
+    res.unshift({ start: 0, end: res[0].start })
+  }
+
   return res.map((item, index) => {
-    let endOfSlice = inputString.length - 1
+    let endOfSlice = inputString.length;
     if (index < res.length - 1) {
       endOfSlice = res[index + 1].start - 1
     }
 
     return {
-      data: inputString.substring(item.start, endOfSlice),
+      data: inputString.substring(item.start, endOfSlice + 1),
       startInParent: item.start,
       startInDoc: item.start,
       endInDoc: endOfSlice,
@@ -147,14 +151,105 @@ type DocumentSlice = {
   endInParent: number
   length: number
   parentSlice: DocumentSlice | null
+  children?: DocumentSlice[]
+}
+
+function handleTable(
+  input: DocumentSlice,
+  patternConfig: { pattern: RegExp; isTable: boolean }
+): DocumentSlice[] {
+  const subSlicePositions = getPositionsOfNeedle(
+    input.data,
+    patternConfig.pattern
+  )
+
+  const tableSlices = subSlicePositions.map((subSlicePosition, index) => {
+    return {
+      data: input.data.substring(subSlicePosition.start, subSlicePosition.end),
+      startInParent: subSlicePosition.start,
+      startInDoc: input.startInDoc + subSlicePosition.start,
+      endInParent: subSlicePosition.end,
+      endInDoc: input.startInDoc + subSlicePosition.end,
+      length: subSlicePosition.end - subSlicePosition.start,
+      parentSlice: input
+    }
+  })
+
+  const slicesAroundTables = tableSlices
+    .map((tableSlice, index) => {
+      //beginning until table
+      const slices = []
+      if (index === 0) {
+        if (tableSlice.startInParent !== 0) {
+          //first table but not at the beginning
+          slices.push({
+            data: input.data.substring(0, tableSlice.startInParent),
+            startInParent: 0,
+            startInDoc: input.startInDoc,
+            endInParent: tableSlice.startInParent,
+            endInDoc: input.startInDoc + tableSlice.startInParent,
+            length: tableSlice.startInParent,
+            parentSlice: input
+          })
+        }
+      } else {
+        if (tableSlices[index - 1].endInParent !== tableSlice.startInParent) {
+          //text between tables
+          slices.push({
+            data: input.data.substring(
+              tableSlices[index - 1].endInParent,
+              tableSlice.startInParent
+            ),
+            startInParent: tableSlices[index - 1].endInParent,
+            startInDoc: input.startInDoc + tableSlices[index - 1].endInParent,
+            endInParent: tableSlice.startInParent,
+            endInDoc: input.startInDoc + tableSlice.startInParent,
+            length:
+              tableSlice.startInParent - tableSlices[index - 1].endInParent,
+            parentSlice: input
+          })
+        }
+      }
+
+      if (index === tableSlices.length - 1) {
+        if (tableSlice.endInParent !== input.data.length) {
+          //last table but not at the end
+          slices.push({
+            data: input.data.substring(
+              tableSlice.endInParent,
+              input.data.length - 1
+            ),
+            startInParent: tableSlice.endInParent,
+            startInDoc: input.startInDoc + tableSlice.endInParent,
+            endInParent: input.data.length - 1,
+            endInDoc: input.startInDoc + input.data.length - 1,
+            length: input.data.length - 1 - tableSlice.endInParent,
+            parentSlice: input
+          })
+        }
+      }
+
+      return slices
+    })
+    .flat()
+
+  tableSlices.push(...slicesAroundTables)
+  tableSlices.sort((a, b) => a.startInParent - b.startInParent)
+
+  input.children = tableSlices
+  return tableSlices
 }
 
 export function prepareLvl2Headings(
   input: DocumentSlice,
   patternConfig: { pattern: RegExp, isTable: boolean }
 ): DocumentSlice[] {
-  const subSlicePositions = getPositionsOfNeedle(input.data, patternConfig.pattern);
 
+  if (patternConfig.isTable) {
+    return handleTable(input, patternConfig)
+  }
+
+  const subSlicePositions = getPositionsOfNeedle(input.data, patternConfig.pattern);
   if (subSlicePositions.length === 0) {
     return [input];
   }
@@ -166,13 +261,13 @@ export function prepareLvl2Headings(
   
   return subSlicePositions.map((subSlicePosition, index) => {
     //this does not work for tables, table can be closed and afterwards there can be text
-    let endOfSubSlice = input.data.length - 1;
+    let endOfSubSlice = input.data.length;
     if (index < subSlicePositions.length - 1) {
       //the start of the next slice is the end of the current slice
       endOfSubSlice = subSlicePositions[index + 1].start - 1
     }
-    
-    return {
+
+    const subSlice = {
       data: input.data.substring(subSlicePosition.start, endOfSubSlice),
       startInParent: subSlicePosition.start,
       startInDoc: input.startInDoc + subSlicePosition.start,
@@ -180,21 +275,37 @@ export function prepareLvl2Headings(
       endInDoc: input.startInDoc + endOfSubSlice,
       length: endOfSubSlice - subSlicePosition.start,
       parentSlice: input
-    }
+    };
+    input.children = [subSlice];
+    return subSlice;
   })
 }
 
 export async function loadSourceV2({
   data = [racesdata, classesdata].join('\n'),
-  idProvider = () => randomUUID()
+  idProvider = () => randomUUID(),
+  maxLength = 1000
 } = {}): Promise<any> {
 
-  const maxLength = 1000;
-  const splitByFirstLvl = await prepareLvl1Headings(data)
-  let currentLevel = [...splitByFirstLvl];
+  const rootDocSlice: DocumentSlice = {
+    data,
+    startInParent: 0,
+    startInDoc: 0,
+    endInDoc: data.length,
+    endInParent: data.length,
+    length: data.length,
+    parentSlice: null,
+    children: []
+  }
+
+  let currentLevel = [rootDocSlice];
   
   let counter = 0;
   const regexes = [
+    {
+      pattern: /.+\n=+\n/g,
+      isTable: false
+    },
     {
       pattern: /.+\n-+\n/g,
       isTable: false
@@ -230,14 +341,14 @@ export async function loadSourceV2({
       isTable: false
     },
     {
-      pattern: /(\|.*\|\r?\n\|[-:|]*\|\r?\n)(\|.*\|\r?\n)*/g,
+      pattern: /(\|.*\|\r?\n\|[-:|]*\|\r?\n)(\|.*\|\r?\n?)*/g,
       isTable: true
     }
   ]
  
   const finalSlices = [];
 
-  while (currentLevel.some(curlvl => curlvl.data.length > maxLength)) {
+  do {
     if(counter > regexes.length -1 || currentLevel.length === 0) {
       finalSlices.push(...currentLevel);
       break;
@@ -252,7 +363,7 @@ export async function loadSourceV2({
       console.log("after lvl " + counter + " we have " + currentLevel.length + " slices left and " + finalSlices.length + " final slices");
 
       counter++;
-  }
+  } while (currentLevel.some(curlvl => curlvl.data.length > maxLength))
 
   return finalSlices;
 
