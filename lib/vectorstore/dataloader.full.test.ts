e3f2ba0efa2595a39b390b4632c6d4f242581ad3
Marius Stein <mariusstein7@gmail.com>
2024-05-17 02:31:10 +0200

    remove log statements

--- lib/vectorstore/dataloader.full.test.ts
+++ lib/vectorstore/dataloader.full.test.ts
@@ -14,8 +14,8 @@ describe('should test dataloader', () => {
     const largeDocs = res
       .filter(item => item.pageContent.length > 1000)
       .sort((a, b) => a.pageContent.length - b.pageContent.length)
-    console.log('large docs (MarkdownSplitter): ' + largeDocs.length)
-    console.log('all docs (MarkdownSplitter): ' + res.length)
+
+    expect(largeDocs.length).toBe(314); // some are allowed to be longer, mainly tables
 
     expect(
       res
